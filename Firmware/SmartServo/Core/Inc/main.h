9ab87a5dc32fca89e6d70de1623af247fbfd4888
Werner <wernertandrade@gmail.com>
2020-09-05 22:47:16 +0100

    Dev board created

---
+++ Firmware/SmartServo/Core/Inc/main.h
@@ -0,0 +1,99 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f3xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define I_Sense_Pin GPIO_PIN_1
+#define I_Sense_GPIO_Port GPIOA
+#define Direction_Pin GPIO_PIN_2
+#define Direction_GPIO_Port GPIOA
+#define PWM_Out_Pin GPIO_PIN_3
+#define PWM_Out_GPIO_Port GPIOA
+#define nSleep_Pin GPIO_PIN_4
+#define nSleep_GPIO_Port GPIOA
+#define nFault_Pin GPIO_PIN_5
+#define nFault_GPIO_Port GPIOA
+#define LED_B_Pin GPIO_PIN_9
+#define LED_B_GPIO_Port GPIOA
+#define PWM_IN_Pin GPIO_PIN_10
+#define PWM_IN_GPIO_Port GPIOA
+#define LED_G_Pin GPIO_PIN_11
+#define LED_G_GPIO_Port GPIOA
+#define LED_R_Pin GPIO_PIN_12
+#define LED_R_GPIO_Port GPIOA
+#define AD0_Pin GPIO_PIN_15
+#define AD0_GPIO_Port GPIOA
+#define AD1_Pin GPIO_PIN_3
+#define AD1_GPIO_Port GPIOB
+#define AD2_Pin GPIO_PIN_4
+#define AD2_GPIO_Port GPIOB
+#define AD3_Pin GPIO_PIN_5
+#define AD3_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ After a long search, I have failed to find a servo motor that does everything I 
 Well, thus was born the idea of G.I.S.Mo (Greatly Inteligent Servo Motor). This project aims to develop a board that can be used to replace the vanilla servo driver boards that comes with cheaper ones such as MG995 and MG996R, giving it more power and making it more usefull for distributed controll projects:
 
 Amongst the desired dream come true features we have the following:
-* Embedded MCU for controll loops (with multiple modes to choose from)
+* Embedded MCU (STM32F302 powered!) for controll loops (with multiple modes to choose from)
 ** Simple position PID loop
 ** MISO controller for speed and position simultaniously (Probably a state space controller)
 ** Torque controll loop
@@ -15,6 +15,21 @@ Amongst the desired dream come true features we have the following:
 * Programable LED's (because they are cool)
 * Absolute magnetic encoder (at least 12bits)
 * Free range of movement (180 degrees is just not enough)
+
+# Organization
+The project contains 3 main blocks: first is the hardware, using [KiCad](https://kicad-pcb.org/), in the Hardware folder you will find 2 subfolders, one for the dev board, which is a big version of the board with some more stuff to facilitate firmware development and debugging, and a second one that contains the G.I.S.Mo board that actually fits in the servo (to be made).
+Then we have the Firmware with two other bits, the [CubeMX](https://www.st.com/en/development-tools/stm32cubemx.html) project so we have everything neatly initialized in our MCU, and the actual firmware and its libs.
+
+
+# Version History
+## Development Board
+*0.0.1 - Initial proposal board for development board schematic and layout.
+** [STM32F302K8 MCU](https://www.st.com/en/microcontrollers-microprocessors/stm32f302k8.html) - Small package and seemingly enough processing power for everything we need.
+** [DRV8876RGTR Driver](https://www.ti.com/store/ti/en/p/product/?p=DRV8876RGTR) - Has more than double the current needed and provides an inbuilt current sensing solution (hopefully good enough)
+## G.I.S.Mo Board
+-
+## Firmware
+-
 
 # FAQ
 I have barely started this project and everytime I tell somebody about it, I get the same questions, so here we go:
