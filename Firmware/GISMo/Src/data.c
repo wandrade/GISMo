a7f0ceee536b6347f13d62aa63f8262217570473
Werner Thomassen Andrade <wernertandrade@gmail.com>
2024-03-22 21:40:49 +0000

    Data changed

--- Firmware/GISMo/Src/data.c
+++ Firmware/GISMo/Src/data.c
@@ -3,11 +3,12 @@
 #include "data.h"
 
 data_register_t data_register;
+structured_data_t *d = &data_register.s;
 
 void init_data(){
 	data_register.s.device_ID = 0x5541;
 	data_register.s.version_major = 0;
 	data_register.s.version_minor = 1;
-	data_register.s.led_mode = 0;
+	data_register.s.led_mode = 1;
 }
 
